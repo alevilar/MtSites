d90ff27d45dae241f065d94ded6632d300f4474e
alevilar <alevilar@gmail.com>
2014-06-12 20:33:26 -0300

    nueva documentacion

---
+++ Config/Schema/sites.php
@@ -0,0 +1,47 @@
+<?php
+
+
+/*
+ *
+ * Using the Schema command line utility
+ * 
+ * copy this file into /app/Config/Schema and
+ * run from console:
+ * cake schema create sites
+ *
+ */
+class SitesSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+
+
+	public $sites = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'alias' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'enabled' => array('type' => 'boolean', 'null' => 'false', 'default' => true),
+		'created' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),		
+	);
+
+
+	public $sites_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'site_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+		),
+	);
+
+
+}

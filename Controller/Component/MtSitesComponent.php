0627c7591689335f6eb796840b9f806226efe615
Ale Vilar <alevilar@gmail.com>
2014-09-08 02:08:48 -0300

    component agregado para separar logica del authorize

---
+++ Controller/Component/MtSitesComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+/**
+ * Authentication control component class
+ *
+ * Binds access control with user authentication and session management.
+ *
+ * @package       Cake.Controller.Component
+ * @link http://book.cakephp.org/2.0/en/core-libraries/components/authentication.html
+ */
+class MtSitesComponent extends Component {
+
+	public function initialize(Controller $controller) {
+		$this->request = $controller->request;
+		
+		if ( array_key_exists( 'tenant', $this->request->params ) ) {
+        	// si el usuario tiene, entre sus sitios al sitio actual, entonces esta autorizado        	
+			CakeSession::write('MtSites.current', $this->request->params['tenant'] );
+        	MtSites::load();
+        	return true;
+        }
+	}
+}
